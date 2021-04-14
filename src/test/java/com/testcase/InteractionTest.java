ea87822e9e8d921e8e72f063c0367ec4f8432c66
PansyLi <2273230712@qq.com>
2021-04-14 08:16:39 +0800

    push

---
+++ src/test/java/com/testcase/InteractionTest.java
@@ -0,0 +1,92 @@
+package com.testcase;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class InteractionTest {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeAll
+    public static void initData(){
+        //Chrome不支持键盘的Action操作
+        System.setProperty("webdriver.gecko.driver", "/Users/pan.li/TestEnv/driver/geckodriver");
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver,5);
+    }
+
+    @Test
+    //使用鼠标点击
+    public void fun(){
+        try {
+        driver.get("http://sahitest.com/demo/clicks.htm");
+        Actions actions = new Actions(driver);
+        WebElement dbl = driver.findElement(By.cssSelector("input[value='dbl click me']"));
+        WebElement click = driver.findElement(By.cssSelector("input[value='click me']"));
+        WebElement right = driver.findElement(By.cssSelector("input[value='right click me']"));
+        actions.contextClick(right);
+        actions.perform();
+        Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    //将鼠标移动到某个元素上
+    public void moveTo(){
+        driver.get("https://www.baidu.com/");
+        Actions actions = new Actions(driver);
+        WebElement setEle = driver.findElement(By.id("s-usersetting-top"));
+        actions.moveToElement(setEle);
+        actions.perform();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void dragTest(){
+        driver.get("http://sahitest.com/demo/dragDropMooTools.htm");
+        Actions actions = new Actions(driver);
+        WebElement source = driver.findElement(By.id("dragger"));
+        WebElement tag = driver.findElement(By.xpath("//*[@class='item'][2]"));
+        actions.dragAndDrop(source, tag);
+        actions.perform();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+    }}
+
+    @Test
+    public void keyBoardTest(){
+    try {
+        driver.get("http://sahitest.com/demo/label.htm");
+        Actions action = new Actions(driver);
+        WebElement uname1 = driver.findElements(By.xpath("//input[@type='textbox']")).get(0);
+        WebElement uname2 = driver.findElements(By.xpath("//input[@type='textbox']")).get(1);
+        uname1.sendKeys("ashin");
+        action.keyDown(Keys.COMMAND).sendKeys("a").keyUp(Keys.COMMAND).perform();
+        action.keyDown(Keys.COMMAND).sendKeys("c").keyUp(Keys.COMMAND).perform();
+        action.keyDown(Keys.COMMAND).sendKeys("y").keyUp(Keys.COMMAND).perform();
+    } catch (Exception e){
+        e.printStackTrace();
+    }
+
+    }
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/com/testcase/waitTest.java
@@ -0,0 +1,68 @@
+package com.testcase;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class waitTest {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeAll
+    public static void initData(){
+        System.setProperty("webdriver.chrome.driver", "/Users/pan.li/TestEnv/driver/chromedriver");
+        driver = new ChromeDriver();
+       // driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver,5);
+    }
+    //隐式等待，规定时间内查找元素，找到了状态不对不可点击，也会报错
+
+    @Test
+    public void login(){
+        driver.get("https://ceshiren.com/latest");
+        driver.findElement(By.xpath("//*[@class='header-buttons']/button[2]")).click();
+        driver.findElement(By.id("login-account-name")).sendKeys("2273230712@qq.com");
+        driver.findElement(By.id("login-account-password")).sendKeys("PLesting121!!!");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Test
+    public void webdriverWaitTest(){
+        driver.get("https://ceshiren.com/latest");
+        driver.findElement(By.xpath("//*[@class='header-buttons']/button[2]")).click();
+        //显式等待的两种方式
+        //一般情况下混合使用显式和隐式，不用sleep
+/*        wait.until(new ExpectedCondition<WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath("//*[@class='header-buttons']/button[2]"));
+            } //当找不到元素需要进一步处理时，使用此方法
+        });*/
+
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='header-buttons']/button[2]")));
+        element.click();
+    }
+
+    @Test
+    public void timeSleepTest(){
+        driver.get("https://ceshiren.com/latest");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.xpath("//*[@class='header-buttons']/button[2]")).click();
+        driver.findElement(By.id("login-account-name")).sendKeys("2273230712@qq.com");
+        driver.findElement(By.id("login-account-password")).sendKeys("PLesting121!!!");
+        driver.findElement(By.id("login-button")).click();
+    }
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+}
